f1507ac2887a8a8e20b6c99979f71b8b1a0b15f6
Jose Roberto <Jrcwrxg@gmail.com>
2023-12-17 20:02:58 -0300

    camada controller - incluidos os metodos de atualização e exclusao

--- ProjetoFinanceiro/ProjetoFinanceiro.Web/Controllers/PessoaController.cs
+++ ProjetoFinanceiro/ProjetoFinanceiro.Web/Controllers/PessoaController.cs
@@ -9,14 +9,20 @@ namespace ProjetoFinanceiro.Web.Controllers
 
         public PessoaController()
         {
+            if (_pessoas == null)
+                CarregarPessoas();
+        }
+        private void CarregarPessoas()
+        {
             _pessoas = new List<PessoaDto>();
 
-            PessoaDto pessoa1 = new PessoaDto {
+            PessoaDto pessoa1 = new PessoaDto
+            {
                 Id = Guid.NewGuid().ToString(),
                 Nome = "Jose Robeto",
                 Email = "jose@gmail.com"
 
-            };    
+            };
 
             _pessoas.Add(pessoa1);
 
@@ -42,14 +48,77 @@ namespace ProjetoFinanceiro.Web.Controllers
         }
 
 
-        public IActionResult Listar()
-        { 
-        return View(_pessoas);
-        }
-
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Listar()
+        {
+            return View(_pessoas);
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Nome, Email")] PessoaDto pessoa)
+        {
+            try
+            {
+                pessoa.Id = Guid.NewGuid().ToString();
+                _pessoas.Add(pessoa);
+
+                return RedirectToAction("Listar");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            PessoaDto pessoa = _pessoas.FirstOrDefault(p => p.Id == id);
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete2(string id)
+        {
+            PessoaDto pessoa = _pessoas.FirstOrDefault(p => p.Id == id);
+            if (pessoa != null)
+                _pessoas.Remove(pessoa);
+
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            PessoaDto pessoa = _pessoas.FirstOrDefault(p => p.Id.Equals(id));
+            
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([Bind("Id", "Nome, Email")] PessoaDto pessoa)
+        {
+
+            PessoaDto pessoapesquisa = _pessoas.FirstOrDefault(p => p.Id.Equals(pessoa.Id));
+
+            if (pessoapesquisa != null)
+            {
+                _pessoas.Remove(pessoapesquisa);
+                _pessoas.Add(pessoa);
+            }
+             
+
+            return RedirectToAction("Listar");
+        }
+
     }
 }
