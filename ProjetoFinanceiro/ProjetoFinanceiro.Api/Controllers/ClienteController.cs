5ad9feffb4d9a0ef8df0c8ae1a18a95b4e212a5e
Jose Roberto <Jrcwrxg@gmail.com>
2023-12-16 21:38:20 -0300

    camada controller - criacao do metodo get

---
+++ ProjetoFinanceiro/ProjetoFinanceiro.Api/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoFinanceiro.Domain.Dtos;
+using ProjetoFinanceiro.Domain.Entities;
+using ProjetoFinanceiro.Services.Service;
+using System.Collections.Generic;
+
+namespace ProjetoFinanceiro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ClienteService _clienteService;
+
+        public ClienteController(ClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        public List<ClienteDto> Get()
+        {
+            try
+            {
+                List<Cliente> clientes = _clienteService.Listar();
+                List<ClienteDto> clienteDtos = 
+                    clientes !=null ? Cliente.ConverterParaDto(clientes) : null;
+                
+                return clienteDtos;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        //public string Get()
+        //{
+        //    return "A aplicacao esta funcionando!";
+        //}
+    }
+}
